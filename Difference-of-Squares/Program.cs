525a720d0be82254258eefeb940edb708715ae5f
Karina Rozenboym <karinarozik1@gmail.com>
2024-10-27 02:38:58 +0300

    Добавьте файлы проекта.

---
+++ Difference-of-Squares/Program.cs
@@ -0,0 +1,20 @@
+﻿public static class DifferenceOfSquares
+{
+    public static int CalculateSquareOfSum(int n)
+    {
+        int sum = n * (n + 1) / 2;
+        return sum * sum;
+    }
+
+    public static int CalculateSumOfSquares(int n)
+    {
+        return n * (n + 1) * (2 * n + 1) / 6;
+    }
+
+    public static int CalculateDifferenceOfSquares(int n)
+    {
+        int squareOfSum = CalculateSquareOfSum(n);
+        int sumOfSquares = CalculateSumOfSquares(n);
+        return squareOfSum - sumOfSquares;
+    }
+}
